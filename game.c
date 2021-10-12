e306071de04a450beec24470d4c85839be31117d
Meng <nzmengzhang@gmail.com>
2021-10-12 19:32:02 +1300

    move all game implementation in to one while loop

--- game.c
+++ game.c
@@ -35,75 +35,69 @@ int main (void)
     uint8_t trap_col = 1;
     uint8_t trap_row = 3;
 
-    while (!(state_isGameOver()))
+    while (1)
     {
+        pacer_wait ();
 
-        // check if current stage completed
-        if (state_isStageComplete ()) {
-            // system_init ();
-            // pacer_init (defaultFreq);
-            mazeDisplay_init ();
+        // check if game is over
+        if (state_isGameOver ()) {
+
+            if (!state_isStageComplete ()) {
+                maze_setStage (FAIL_SYMBOL);
+            } else if (state_isStageComplete ()) {
+                maze_setStage (WIN_SYMBOL);
+            }
+        }
+
+        // change to next stage if isStageComplete
+        if (!(state_isGameOver ()) && (state_isStageComplete ())) {
             maze_setStage (maze_stageName () + 1);
             player_init (maze_playerStartCol(), maze_playerStartRow());
             state_init ();
         }
 
-        pacer_wait ();
-
         // run display
+        mazeDisplay_init ();
         maze_display ();
 
-        // run trap
-        if (maze_stageName () == STAGE_1) {
-            trap_tick++;
-            if (trap_tick >= trap_tick_max) {
-                trap_tick = 0;
-                maze_toggleDot (trap_col, trap_row);
+        if (!(state_isGameOver())) {
+            // run trap
+            if (maze_stageName () == STAGE_1) {
+                trap_tick++;
+                if (trap_tick >= trap_tick_max) {
+                    trap_tick = 0;
+                    maze_toggleDot (trap_col, trap_row);
+                }
             }
-        }
 
-        // move player
-        nav_tick++;
-        if (nav_tick >= nav_tick_max) {
-            nav_tick = 0;
-            if (player_move ()) {
-                // turn off player's previous location
-                maze_setDot (player_previousCol (), player_previousRow (), false);
+            // move player
+            nav_tick++;
+            if (nav_tick >= nav_tick_max) {
+                nav_tick = 0;
+                if (player_move ()) {
+                    // turn off player's previous location
+                    maze_setDot (player_previousCol (), player_previousRow (), false);
 
-                // set stage completed if player move into complete location
-                if ((player_col () == maze_playerFinishCol ()) &&
-                        player_row () == maze_playerFinishRow ()) {
-                    state_completeStage ();
+                    // set stage completed if player move into complete location
+                    if ((player_col () == maze_playerFinishCol ()) &&
+                            player_row () == maze_playerFinishRow ()) {
+                        state_completeStage ();
 
-                    if (maze_stageName () >= (FAIL_SYMBOL - 1)) {
+                        if (maze_stageName () >= (FAIL_SYMBOL - 1)) {
+                            state_endGame ();
+                        }
+                    }
+
+                    // set fail symbol display if player move into wall or traps
+                    // or crash into boundaries twice
+                    if (maze_dotState (player_col (), player_row ())) {
                         state_endGame ();
                     }
+
+                    // turn on player's current location
+                    maze_setDot (player_col (), player_row (), true);
                 }
-
-                // set fail symbol display if player move into wall or traps
-                // or crash into boundaries twice
-                if (maze_dotState (player_col (), player_row ())) {
-                    state_endGame ();
-                }
-
-                // turn on player's current location
-                maze_setDot (player_col (), player_row (), true);
-
             }
-        }
-    }
-
-    // End Game
-    mazeDisplay_init ();
-    if (state_isGameOver() && !state_isStageComplete()) {
-        maze_setStage (FAIL_SYMBOL);
-        while (1) {
-            maze_display ();
-        }
-    } else if (state_isGameOver() && state_isStageComplete()) {
-        maze_setStage (WIN_SYMBOL);
-        while (1) {
-            maze_display ();
         }
     }
 }
